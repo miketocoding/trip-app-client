fcb1225cf768b2b67ad6a59d05dcaa6845c24ea1
MiketoCoding <le.michael.v@gmail.com>
2021-07-18 19:09:34 -0400

    Non working react-chartjs-2

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,10 +1,18 @@
 import React, { Fragment } from 'react'
+import { Bar } from 'react-chartjs-2'
 
 const Stats = () => {
   return (
     <Fragment>
       <h4>You are on the Stats Page</h4>
       <p>Below you will see graphs</p>
+      <Bar
+        data={{
+          labels: ['Red', 'Blue', 'Yellow', 'green', 'Purple', 'Orange']
+        }}
+        height={400}
+        width={600}
+      />
     </Fragment>
   )
 }
